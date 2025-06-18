a748fe3f89cd6ff99e279b3d26ff4ade2ef1865e
suvosree-tech <suvosreepersonal@gmail.com>
2025-06-18 22:38:13 +0530

    design frontend and file structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=" flex min-h-screen w-full min-h-screen justify-center items-center">
+    <div className="w-1/2 text-center">
+      <h1 className="text-lg text-balance">👋 Welcome to Twitter Automation AI – your intelligent partner for social media success.</h1>
+      <p className="text-gray-500">We’re delighted to support your journey toward a more streamlined, impactful Twitter strategy. From AI-driven tweet creation to smart scheduling and engagement insights, we’re here to help you grow – efficiently and effectively.
+Let’s elevate your Twitter presence together. </p>
+<form action="" className="flex justify-around">
+  <input type="text" className="border-2 w-4/5 focus:border-2 focus:" />
+  <button className="p-2"><svg height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376l0 103.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg></button>
+</form>
+    </div>
+    </div>
+  )
+}
+
+export default App
