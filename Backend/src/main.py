f36c48c9457bc359da5179967685d65712e4dba4
suvosree-tech <suvosreepersonal@gmail.com>
2025-06-28 15:58:53 +0530

    push

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -19,7 +19,7 @@ app=FastAPI(
 app.middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "http://localhost:5173/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
